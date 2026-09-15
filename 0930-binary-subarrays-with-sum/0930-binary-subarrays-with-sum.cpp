c2146a4ab6f13ff7a1c05a40abf69b1a46d799a0
chandu <chanduatp12@gmail.com>
2026-09-15 21:01:16 +0530

    Time: 18 ms (29.88%), Space: 38.8 MB (12.36%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -11,18 +11,18 @@ public:
         //     }
         // }
         // return cnt;
-       unordered_map<int,int>mp;
-       mp[0]=1;
-       int cnt=0;
-       int sum=0;
-       for(int i=0;i<nums.size();i++){
-sum+=nums[i];
-int needed=sum-goal;
-if(mp.find(needed)!=mp.end()){
-    cnt+=mp[needed];
-}
-mp[sum]++;
-       }
-       return cnt;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int cnt = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            int needed = sum - goal;
+            if (mp.find(needed) != mp.end()) {
+                cnt += mp[needed];
+            }
+            mp[sum]++;
+        }
+        return cnt;
     }
 };
